b7777bf9c97e4ad33a77509b24cf344438a4715b
hryntar <hrynchuktt@gmail.com>
2024-08-09 15:54:21 +0300

    route params

--- src/index.mjs
+++ src/index.mjs
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
    res.send({ message: "Hello World" });
 })
 
+app.get("/api/users/:id/:username", (req, res) => {
+   console.log(req.params);
+})
+
 app.listen(PORT, () => {
    console.log(`Running on port ${PORT}`);
 });
