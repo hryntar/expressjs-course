148f68f23d15f3aeb9a87755f4df4d0b7ed57618
hryntar <hrynchuktt@gmail.com>
2024-08-30 14:51:17 +0300

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,18 @@
 import express from "express";
-import routes from "./routes/index.mjs";
+import routes from "./routes/index.mjs"; 
+import cookieParser from "cookie-parser";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
+   console.log(req.cookies);
+   res.cookie("hello", "world", { maxAge: 60000 * 60 * 2})
    res.send({ message: "Hello World" });
 });
 
